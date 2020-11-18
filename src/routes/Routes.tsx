8437506205e1678de41b17262a181ab9f4e83088
Pablo Roman Lorente <proman@plainconcepts.com>
2020-11-18 18:21:26 +0100

    added separate loading component to all pages

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,24 +1,27 @@
 import React, { Component } from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
 
-import Index from "../pages/index";
+import Dashboard from "../pages/index/dashboard";
 import Home from "../pages/sidebar/home";
 import Security from "../pages/sidebar/security";
 import Lights from "../pages/sidebar/lights";
 import Garden from "../pages/sidebar/garden";
 import Cleaning from "../pages/sidebar/cleaning";
+import Profile from "../pages/sidebar/profile";
+
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
-                <Route exact path="/" render={() => <Redirect to="/index"/>} />
-                <Route path='/index' exact component={Index}/>
+                <Route exact path="/" render={() => <Redirect to="/dashboard"/>} />
+                <Route path='/dashboard' exact component={Dashboard}/>
                 <Route path='/home' exact component={Home}/>
                 <Route path='/security' exact component={Security}/>
                 <Route path='/lights' exact component={Lights}/>
                 <Route path='/garden' exact component={Garden}/>
                 <Route path='/cleaning' exact component={Cleaning}/>
+                <Route path='/profile' exact component={Profile}/>
           </Switch>
         )
     }
