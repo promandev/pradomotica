516a124c69f9143664dec9dc64c031b471193320
Pablo Roman Lorente <proman@plainconcepts.com>
2020-11-18 15:00:47 +0100

    index and home pages modified

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,22 +1,24 @@
 import React, { Component } from "react"
 import { Switch, Route, Redirect } from "react-router-dom"
 
-import Cleaning from "../pages/sidebar/cleaning";
-import Garden from "../pages/sidebar/garden";
+import Index from "../pages/index";
 import Home from "../pages/sidebar/home";
-import Lights from "../pages/sidebar/lights";
 import Security from "../pages/sidebar/security";
+import Lights from "../pages/sidebar/lights";
+import Garden from "../pages/sidebar/garden";
+import Cleaning from "../pages/sidebar/cleaning";
 
 class Routes extends Component {
     render() {
         return (
             <Switch>
-            <Route exact path="/" render={() => <Redirect to="/dashboard"/>} />
-            <Route path='/' exact component={Home}/>
-            <Route path='/security' exact component={Security}/>
-            <Route path='/lights' exact component={Lights}/>
-            <Route path='/garden' exact component={Garden}/>
-            <Route path='/cleaning' exact component={Cleaning}/>
+                <Route exact path="/" render={() => <Redirect to="/index"/>} />
+                <Route path='/index' exact component={Index}/>
+                <Route path='/home' exact component={Home}/>
+                <Route path='/security' exact component={Security}/>
+                <Route path='/lights' exact component={Lights}/>
+                <Route path='/garden' exact component={Garden}/>
+                <Route path='/cleaning' exact component={Cleaning}/>
           </Switch>
         )
     }
