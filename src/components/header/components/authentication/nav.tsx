31d4d195d565213a3b6142ce4a2cb14cf4092f60
Pablo Roman Lorente <proman@plainconcepts.com>
2020-11-21 18:03:45 +0100

    styles applied

--- src/components/header/components/authentication/nav.tsx
+++ src/components/header/components/authentication/nav.tsx
@@ -1,10 +1,10 @@
 import { useAuth0 } from "@auth0/auth0-react"
 import React from "react"
 
-export const Nav = () => {
+export const Nav = ():any => {
     const { loginWithPopup, logout, isAuthenticated } = useAuth0();
     
-    return !isAuthenticated ? (<button className="authentication-buttons__signup">Sign Up</button> && <button onClick={() => loginWithPopup()} className="authentication-buttons__login">Login</button>) : (<button onClick={() => logout()} className="authentication-buttons__logout">Logout</button>)
+    return !isAuthenticated ? [(<span><button className="authentication-buttons__signup">Sign Up</button></span>), (<span><button onClick={(e) => loginWithPopup()} className="authentication-buttons__login">Login</button></span>)] : (<span><button onClick={(e) => logout()} className="authentication-buttons__logout">Logout</button></span>)
 }
 
 export default Nav;
