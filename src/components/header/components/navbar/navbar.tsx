ea399abf0e29eae5a412ad1e9665ef85e5622381
Pablo Roman Lorente <proman@plainconcepts.com>
2021-03-01 12:46:43 +0100

    navbar opacity modified

--- src/components/header/components/navbar/navbar.tsx
+++ src/components/header/components/navbar/navbar.tsx
@@ -30,23 +30,27 @@ function Navbar() {
 
         <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
             <ul className="nav-menu-items" onClick={showSidebar}>
-                <li className="navbar-toggle">
-                    <Link to="#" className="menu-bars">
-                        <AiIcons.AiOutlineClose/>
-                    </Link>
-                </li>
-                {SidebarData.map((item, index) => {
-                    return(
-                        <li key={index} className={item.cName}>
-                            <Link to={item.path}>
-                                {item.icon}
-                                <span>
-                                    {item.title}
-                                </span>
-                            </Link>
-                        </li>
-                    )
-                })}
+                <div className='navbar-topSide'>
+                    <li className="navbar-toggle">
+                        <Link to="#" className="menu-bars">
+                            <AiIcons.AiOutlineClose/>
+                        </Link>
+                    </li>                
+                </div>
+                <div className='navbar-bottomSide'>
+                    {SidebarData.map((item, index) => {
+                        return(
+                            <li key={index} className={item.cName}>
+                                <Link to={item.path}>
+                                    {item.icon}
+                                    <span>
+                                        {item.title}
+                                    </span>
+                                </Link>
+                            </li>
+                        )
+                    })}
+                </div>
             </ul>
         </nav>
 
