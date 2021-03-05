a02b195d59762c0c75d7c7858779c0928b4d661b
Pablo Roman Lorente <proman@plainconcepts.com>
2021-03-05 13:22:45 +0100

    merged to prevent errors

--- src/components/body/components/homeView/calendarWidget.tsx
+++ src/components/body/components/homeView/calendarWidget.tsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
+
 // import 'react-calendar/dist/Calendar.css';
 
 function CalendarWidget() {
   const [selectedDate, setSelectedDate] = useState<Date|Date[]|any>(null);
 
   return (
-    <div className="calendarWidget">
+    <div className="react-calendar">
       <Calendar
         onChange={setSelectedDate}
         value={selectedDate}
